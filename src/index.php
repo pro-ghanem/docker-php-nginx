c99f54990db4943bcce257cfe983616d2e00e714
pro-ghanem <holywater150@gmail.com>
2020-09-03 19:51:10 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,14 +1,51 @@
 <html>
    
    <head>
-      <title>ghanem</title>
+      <title>Add New Record in MySQL Database</title>
    </head>
    
    <body>
-         <h>
-      ghanem
-   </h>
+<?php
+$servername = "mysql"; # service name
+$username = "db_user";
+$password = "ghanem";
+$dbname="mydb";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
+// Create database
+$sql = "CREATE DATABASE ghanem";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  $last_id = $conn->insert_id;
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+     
+     
+$conn->close();
+?>
    
    </body>
 </html>
